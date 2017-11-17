4806128af80ec787aeed5523964b4680a9055c1f
elhenro <github@henry-schober.com>
2017-11-17 02:08:55 +0100

    try5

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join('dist', 'index.html'));
 });
 
 var port = process.env.PORT || '8000';
